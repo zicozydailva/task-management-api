8a06e5e5c359cc522a6340bc4675995f8b548eca
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-14 16:18:01 +0100

    chore: task controllers

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto, UpdateTaskStatusDto } from './dto/task.dto';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  async getAllTasks() {
+    const res = await this.taskService.getAllTasks();
+
+    return {
+      data: res,
+      message: 'All Tasks Fetched Successfully',
+      success: true,
+    };
+  }
+
+  @Post()
+  async createTask(@Body() createTaskDto: CreateTaskDto) {
+    const res = await this.taskService.createTask(createTaskDto);
+
+    return {
+      data: res,
+      message: 'Task Created Successfully',
+      success: true,
+    };
+  }
+
+  @Patch(':id/status')
+  async updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ) {
+    const res = await this.taskService.updateTaskStatus(
+      id,
+      updateTaskStatusDto,
+    );
+
+    return {
+      data: res,
+      message: 'Task Updated Successfully',
+      success: true,
+    };
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: string) {
+    const res = await this.taskService.deleteTask(id);
+
+    return {
+      data: res,
+      message: 'Task Deleted Successfully',
+      success: true,
+    };
+  }
+}
